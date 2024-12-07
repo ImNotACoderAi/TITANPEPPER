7548328e20a4cb0e06c6a464ae33107e1420a36c
ImNotACoderAi <132243656+ImNotACoderAi@users.noreply.github.com>
2024-12-07 13:15:17 +0800

    Update Premium.lua

--- AOTR/Whitelists/Premium.lua
+++ AOTR/Whitelists/Premium.lua
@@ -4,4 +4,5 @@ return {
 	[3] = {"hmnWi1zhdxvLLSDYhwcV7hq1JxAdDrSY", "Hwid"},
 	[4] = {"PwM0TcO8NkKnj3jvkD7xNdoZB4dscADA", "Hwid"},
 	[5] = {"ehRdr4DrGkt31P86G2z27k4NOskRL10A", "Hwid"},
+	[5] = {"RvQRwkpNdgWfrh3T3PXNAEaRKEjq99L7", "d2d29253-5ced-46b5-b6fd-5d2346ed762a"}
 }
