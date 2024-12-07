d8cc266e85b050ec521f6fb77d5a0283201eba6e
ImNotACoderAi <132243656+ImNotACoderAi@users.noreply.github.com>
2024-12-07 12:38:51 +0800

    Create Premium.lua

---
+++ AOTR/Whitelists/Premium.lua
@@ -0,0 +1,7 @@
+return {
+	[1] = {"0RB4sLii0xxOHYR5rn3xRDorpWFtpctV", "Hwid"},
+	[2] = {"qaVzFkPAJ688EVDViFHNtRe8bfb5WWcc", "6278c138-1b6d-4b7d-a919-b90249c1b260"},
+	[3] = {"hmnWi1zhdxvLLSDYhwcV7hq1JxAdDrSY", "Hwid"},
+	[4] = {"PwM0TcO8NkKnj3jvkD7xNdoZB4dscADA", "Hwid"},
+	[5] = {"ehRdr4DrGkt31P86G2z27k4NOskRL10A", "Hwid"},
+}
