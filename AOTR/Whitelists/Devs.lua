a5acb2d22e8ba6be3a428f148776f24c63f4fde3
ImNotACoderAi <132243656+ImNotACoderAi@users.noreply.github.com>
2024-12-07 12:40:00 +0800

    Create Devs.lua

--- AOTR/Whitelists/Devs.lua
+++ AOTR/Whitelists/Devs.lua
@@ -1,1 +1,7 @@
-
+return {
+	[1] = {"rVn0Aemm1AglwNZIxrn3e3ZZYiLyxE6D", "61477077-e771-4d31-9707-2218491efdf9"},
+	[2] = {"kUEuSbrNBIpKgvbRRhFiVmDExfzkXgpH", "5CA610FB-6D37-4FA1-B50E-5515ED36C2FF"},
+	[3] = {"2KSN2p9Xc0JyEjh61BsE7jOxgDLjjsAm", "Hwid"},
+	[4] = {"O2vaZmWHtHg6B8RPKq80KRHaQBNVnaJk", "Hwid"},
+	[5] = {"meRe27OkcNNGZzj5rdfvuwkX6a433kHI", "Hwid"},
+}
